1472421f2b0a0ca35342e29c00a550bb5ef2805b
Danya <dalfakih@yahoo.com>
2019-04-07 00:46:39 -0700

    Added objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,34 @@
-var cards = ["Queen","Queen","King","King"];
+// cards = ["Queen","Queen","King","King"];
 var cardsInPlay =[];
+var cards =[
+	{
+		rank : "queen" ,
+	    suit : "hearts"	,
+	    cardImage : "images/queen-of-hearts.png"
+	} 
+	,
+
+	{
+		rank : "queen" ,
+	    suit : "diamonds"	,
+	    cardImage : "images/queen-of-diamonds.png"
+	} 
+	,
+	{
+		rank : "king" ,
+	    suit : "hearts"	,
+	    cardImage : "images/king-of-hearts.png"
+	} 
+	,
+	{
+		rank : "king" ,
+	    suit : "diamonds"	,
+	    cardImage : "images/king-of-diamonds.png"
+	} 
+
+
+
+]
 
 var checkForMatch = function(){
 
@@ -12,9 +41,11 @@ var checkForMatch = function(){
 
 var flipCard = function(cardId){
 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 
 
