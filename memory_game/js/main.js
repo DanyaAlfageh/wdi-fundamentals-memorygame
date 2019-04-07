e6bedc28d466b253e5f64bedf2de51dc7400210e
Danya <dalfakih@yahoo.com>
2019-04-06 23:09:20 -0700

    created a function

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,24 @@
 var cards = ["Queen","Queen","King","King"];
 var cardsInPlay =[];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User Flipped Queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User Flipped King");
-if(cardsInPlay.length===2){
-	if(cardsInPlay[0]===cardsInPlay[1])
-		alert("You found a match!");
-	else alert("Sorry, try again.");
+
+var checkForMatch = function(){
+
+	    if(cardsInPlay.length===2){
+		if(cardsInPlay[0]===cardsInPlay[1])
+				alert("You found a match!");
+		else alert("Sorry, try again.");
+    }
 }
+
+var flipCard = function(cardId){
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+
+
+
+}
+
+flipCard(0);
+flipCard(2);
